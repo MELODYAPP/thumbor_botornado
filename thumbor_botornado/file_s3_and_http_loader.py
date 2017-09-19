c87a3e4fa5a5459bc1a4ba7e386bba403be61b6d
Jonathan Rhone <rhone.j@gmail.com>
2017-09-19 11:54:11 -0400

    Add loader that checks file, s3, and http and in that order

---
+++ thumbor_botornado/file_s3_and_http_loader.py
@@ -0,0 +1,27 @@
+from tornado.concurrent import return_future
+import thumbor_botornado.s3_loader
+import thumbor.loaders.http_loader
+import thumbor.loaders.file_loader
+import re
+
+HTTP_RE = re.compile(r'\Ahttps?:', re.IGNORECASE)
+S3_RE = re.compile(r'\Ahttps?://.*-melody.s3.amazonaws.com/(?P<path>.*)', re.IGNORECASE)
+
+
+@return_future
+def load(context, url, callback):
+    match = S3_RE.match(url)
+
+    def callback_wrapper(result):
+        if result.successful:
+            callback(result)
+        else:
+            # If not on efs, try s3
+            s3_loader.load(context, match.group('path'), callback)
+
+    # If melody s3 file, first try to load from efs
+    if match:
+        file_loader.load(context, match.group('path'), callback_wrapper)
+    # else get from the internet
+    elif:
+        http_loader.load(context, url, callback)

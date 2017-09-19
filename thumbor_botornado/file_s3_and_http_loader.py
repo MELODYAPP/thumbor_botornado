f73f26178ae063a92ae3fccf5b938208ea9acda5
Jonathan Rhone <rhone.j@gmail.com>
2017-09-19 12:06:10 -0400

    fix

--- thumbor_botornado/file_s3_and_http_loader.py
+++ thumbor_botornado/file_s3_and_http_loader.py
@@ -1,3 +1,4 @@
+import os
 from tornado.concurrent import return_future
 import thumbor_botornado.s3_loader
 import thumbor.loaders.http_loader
@@ -5,7 +6,7 @@ import thumbor.loaders.file_loader
 import re
 
 HTTP_RE = re.compile(r'\Ahttps?:', re.IGNORECASE)
-S3_RE = re.compile(r'\Ahttps?://.*-melody.s3.amazonaws.com/(?P<path>.*)', re.IGNORECASE)
+S3_RE = re.compile(r'\Ahttps?://(?P<bucket>.*).s3.amazonaws.com/(?P<path>.*)', re.IGNORECASE)
 
 
 @return_future
@@ -17,7 +18,9 @@ def load(context, url, callback):
             callback(result)
         else:
             # If not on efs, try s3
-            s3_loader.load(context, match.group('path'), callback)
+            s3_loader.load(context,
+                           os.path.join(match.group('bucket'), match.group('path')),
+                           callback)
 
     # If melody s3 file, first try to load from efs
     if match:
